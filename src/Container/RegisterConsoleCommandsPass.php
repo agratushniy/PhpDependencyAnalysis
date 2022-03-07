6e50bc8aafb60ad7d6395c5ec917121011a00875
agratushniy <ag.ratushniy@gmail.com>
2022-03-07 19:27:44 +0500

    Настроена консоль

---
+++ src/Container/RegisterConsoleCommandsPass.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpDA;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Symfony\Component\DependencyInjection\Reference;
+
+final class RegisterConsoleCommandsPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        $consoleApplication = $container->findDefinition(Application::class);
+
+        foreach ($container->findTaggedServiceIds('console_command') as $commandId => $tags) {
+            $consoleApplication->addMethodCall('add', [new Reference($commandId)]);
+        }
+    }
+}
